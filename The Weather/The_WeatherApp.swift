2f01996d555713265c16951d710257b92ec1fd98
Sean Blake <sean.blake97@outlook.com>
2022-04-03 09:12:43 -0400

    Initial Commit

---
+++ The Weather/The_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_WeatherApp.swift
+//  The Weather
+//
+//  Created by SEAN BLAKE on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct The_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
